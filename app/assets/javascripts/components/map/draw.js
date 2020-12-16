02636c856a1b166befd0a3b6d72365379b5bc03e
danleech-defra <danleech-defra@gmail.com>
2020-12-16 17:35:38 +0000

    Stuff

--- app/assets/javascripts/components/map/draw.js
+++ app/assets/javascripts/components/map/draw.js
@@ -97,7 +97,7 @@ function DrawMap (placeholderId, options) {
       small: 'data:image/svg+xml,%3Csvg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32"%3E%3Ccircle cx="16" cy="16" r="4" style="fill:%230b0c0c"/%3E%3C/svg%3E',
       large: 'data:image/svg+xml,%3Csvg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32"%3E%3Ccircle cx="16" cy="16" r="7" style="fill:white;fill-opacity:0.01;stroke:black;stroke-width:2px;"/%3E%3C/svg%3E'
     }
-    // Line styles
+    // Line segments
     if (feature.getGeometry().getType() === 'Polygon') {
       const coordinates = feature.getGeometry().getCoordinates()[0]
       if (coordinates.length > 2) {
@@ -117,7 +117,7 @@ function DrawMap (placeholderId, options) {
         }))
       }
     }
-    // Style points
+    // Points
     styles.push(new Style({
       // Return the coordinates of the first ring of the polygon
       geometry: (feature) => {
