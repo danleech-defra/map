a4b67a908fc4b7a84ad9e4b364462a6fd1ead4b0
danleech-defra <danleech-defra@gmail.com>
2020-12-16 17:33:37 +0000

    Stuff

--- app/assets/javascripts/components/map/draw.js
+++ app/assets/javascripts/components/map/draw.js
@@ -93,30 +93,31 @@ function DrawMap (placeholderId, options) {
   // Styles for drawing a new shape
   const drawStyles = (feature) => {
     const styles = []
-    const coordinates = feature.getGeometry().getCoordinates()
     const image = {
       small: 'data:image/svg+xml,%3Csvg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32"%3E%3Ccircle cx="16" cy="16" r="4" style="fill:%230b0c0c"/%3E%3C/svg%3E',
       large: 'data:image/svg+xml,%3Csvg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32"%3E%3Ccircle cx="16" cy="16" r="7" style="fill:white;fill-opacity:0.01;stroke:black;stroke-width:2px;"/%3E%3C/svg%3E'
     }
-    // const src = feature.getGeometry().getType() === 'Point' ? image.large : image.small
     // Line styles
-    if (feature.getGeometry().getType() === 'LineString') {
-      styles.push(new Style({
-        geometry: new LineString(coordinates.slice(-2)),
-        // fill: new Fill({ color: 'rgba(255, 255, 255, 0.5)' }),
-        stroke: new Stroke({ color: '#b1b4b6', width: 3 }), // lineCap: 'butt', lineDash: [3, 2]
-        zIndex: 2
-      }))
+    if (feature.getGeometry().getType() === 'Polygon') {
+      const coordinates = feature.getGeometry().getCoordinates()[0]
       if (coordinates.length > 2) {
+        // Unplotted segment
         styles.push(new Style({
-          geometry: new LineString(coordinates.slice(0, -1)),
+          geometry: new LineString(coordinates.slice(coordinates.length - 3, coordinates.length - 1)),
           // fill: new Fill({ color: 'rgba(255, 255, 255, 0.5)' }),
-          stroke: new Stroke({ color: '#0b0c0c', width: 3 }),
-          zIndex: 2
+          stroke: new Stroke({ color: '#b1b4b6', width: 3 }), // lineCap: 'butt', lineDash: [3, 2]
+          zIndex: 1
+        }))
+        // Plotted segment
+        styles.push(new Style({
+          geometry: new LineString(coordinates.slice(0, coordinates.length - 2)),
+          // fill: new Fill({ color: 'rgba(255, 255, 255, 0.5)' }),
+          stroke: new Stroke({ color: '#0b0c0c', width: 3 }), // lineCap: 'butt', lineDash: [3, 2]
+          zIndex: 1
         }))
       }
     }
-    // Style lines
+    // Style points
     styles.push(new Style({
       // Return the coordinates of the first ring of the polygon
       geometry: (feature) => {
